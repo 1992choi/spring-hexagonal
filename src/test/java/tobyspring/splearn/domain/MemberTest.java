614de549aefa83678533a00561e1f5b85068a890
choi <younggeunn@naver.com>
2025-07-04 22:45:16 +0900

    섹션 4. Member 도메인 로직 개발

--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -1,7 +1,9 @@
 package tobyspring.splearn.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class MemberTest {
 
@@ -9,7 +11,47 @@ class MemberTest {
     void createMember() {
         var member = new Member("toby@splearn.app", "Toby", "secret");
 
-        Assertions.assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+    @Test
+    void activate() {
+        var member = new Member("toby@splearn.app", "Toby", "secret");
+
+        member.activate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+
+    @Test
+    void activateFail() {
+        var member = new Member("toby@splearn.app", "Toby", "secret");
+        member.activate();
+
+        assertThatThrownBy(() -> {
+            member.activate();
+        }).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void deactivate() {
+        var member = new Member("toby@splearn.app", "Toby", "secret");
+        member.activate();
+
+        member.deactivate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+    }
+
+    @Test
+    void deactivateFail() {
+        var member = new Member("toby@splearn.app", "Toby", "secret");
+        assertThatThrownBy(() -> member.deactivate()).isInstanceOf(IllegalStateException.class);
+
+        member.activate();
+        member.deactivate();
+
+        assertThatThrownBy(() -> member.deactivate()).isInstanceOf(IllegalStateException.class);
     }
 
 }
