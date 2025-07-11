10087f587f4d9bbc7a88817fe6dec3c59dc5e931
choi <younggeunn@naver.com>
2025-07-11 22:12:16 +0900

    섹션 4. 회원 애플리케이션 서비스 테스트 (2)

--- src/main/java/tobyspring/splearn/application/required/MemberRepository.java
+++ src/main/java/tobyspring/splearn/application/required/MemberRepository.java
@@ -1,11 +1,16 @@
 package tobyspring.splearn.application.required;
 
 import org.springframework.data.repository.Repository;
+import tobyspring.splearn.domain.Email;
 import tobyspring.splearn.domain.Member;
+
+import java.util.Optional;
 
 /**
  * 회원 정보를 저장하거나 조회한다
  */
 public interface MemberRepository extends Repository<Member, Long> { // 특이사항 : JpaRepository 대신 Repository 사용
     Member save(Member member);
+
+    Optional<Member> findByEmail(Email email);
 }
