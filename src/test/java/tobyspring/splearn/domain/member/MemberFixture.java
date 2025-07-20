d949547bda7adbb202e88128f13a0f5b3db9de18
choi <younggeunn@naver.com>
2025-07-20 10:43:09 +0900

    섹션 8. MemberApi와 웹 단위 테스트

--- src/test/java/tobyspring/splearn/domain/member/MemberFixture.java
+++ src/test/java/tobyspring/splearn/domain/member/MemberFixture.java
@@ -1,5 +1,7 @@
 package tobyspring.splearn.domain.member;
 
+import org.springframework.test.util.ReflectionTestUtils;
+
 public class MemberFixture {
     public static MemberRegisterRequest createMemberRegisterRequest(String email) {
         return new MemberRegisterRequest(email, "Charlie", "verysecret");
@@ -21,6 +23,12 @@ public class MemberFixture {
                 return encode(password).equals(passwordHash);
             }
         };
+    }
+
+    public static Member createMember(Long id) {
+        Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        ReflectionTestUtils.setField(member, "id", id);
+        return member;
     }
 
 }
